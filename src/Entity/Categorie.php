0dda467feb1fd9e291c543d14deda5356d6f93f5
Asus <aminimaadii@gmail.com>
2023-06-14 12:19:25 +0100

    Add existing project files to Git

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $id_categorie;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $nomCategorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdCategorie(): ?int
+    {
+        return $this->id_categorie;
+    }
+
+    public function setIdCategorie(?int $id_categorie): self
+    {
+        $this->id_categorie = $id_categorie;
+
+        return $this;
+    }
+
+    public function getNomCategorie(): ?string
+    {
+        return $this->nomCategorie;
+    }
+
+    public function setNomCategorie(string $nomCategorie): self
+    {
+        $this->nomCategorie = $nomCategorie;
+
+        return $this;
+    }
+}
